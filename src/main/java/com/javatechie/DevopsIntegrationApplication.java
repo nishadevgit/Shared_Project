5dce80ba49f1b2230ce3147d2847a69a55c7d604
nishadevgit <147315014+nishadevgit@users.noreply.github.com>
2024-06-16 01:12:48 -0400

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project Hi from Nisha!!";
+		return "Welcome to our DevOps Project Hi from Nisha second attempt!!";
 	}
 
 	public static void main(String[] args) {
